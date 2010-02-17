900e741cc21e508cef6d08c51e3a8b6ceeeac309
fkooman@tuxed.net <fkooman@tuxed.net@70712f9e-167c-11df-aa5a-d92c9819c6ea>
2010-02-17 15:12:17 +0000

    fixup the release script and run scripts some more

--- trunk/misc/NokiCert-Mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-CLASSPATH=${CLASSPATH}:bluecove.jar
-CLASSPATH=${CLASSPATH}:mac/swt.jar
-CLASSPATH=${CLASSPATH}:nokicert.jar
-
-java -XstartOnFirstThread -d32 -cp ${CLASSPATH} net.tuxed.nokicert.NokiCertGUI
--- trunk/misc/NokiCert.sh
+++ trunk/misc/NokiCert.sh
@@ -3,27 +3,16 @@
 # OS
 OS=`uname -s`
 # Hardware Platform
-HW=`uname -p`
+HW=`uname -m`
 
 CLASSPATH=${CLASSPATH}:bluecove.jar
 CLASSPATH=${CLASSPATH}:nokicert.jar
 
 if [ $OS == "Linux" ]
-	then
-	
-	if [ $HW == "i686" ]
-	then
-		# i686
-		CLASSPATH=${CLASSPATH}:linux32/bluecove-gpl.jar
-		CLASSPATH=${CLASSPATH}:linux32/bluecove-bluez.jar
-		CLASSPATH=${CLASSPATH}:linux32/swt.jar
-	elif [ $HW == "x86_64" ]
-	then	
-		# x86_64
-		CLASSPATH=${CLASSPATH}:linux64/bluecove-gpl.jar
-		CLASSPATH=${CLASSPATH}:linux64/bluecove-bluez.jar
-		CLASSPATH=${CLASSPATH}:linux64/swt.jar
-	fi
+then
+	CLASSPATH=${CLASSPATH}:linux-$HW/bluecove-gpl.jar
+	CLASSPATH=${CLASSPATH}:linux-$HW/bluecove-bluez.jar
+	CLASSPATH=${CLASSPATH}:linux-$HW/swt.jar
 	java -cp ${CLASSPATH} net.tuxed.nokicert.NokiCertGUI
 elif [ $OS == "Darwin" ] 
 then
